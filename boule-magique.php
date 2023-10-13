e0a92b96959e0b47a9136769133fca4f91837f39
Julia-Legrand <julia-legrand@orange.fr>
2023-10-13 17:30:04 +0200

    Mise à jour du code

--- boule-magique.php
+++ boule-magique.php
@@ -15,25 +15,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Boule magique</title>
 
+    <!--ICONS-->
+    <script src="https://kit.fontawesome.com/d977b1f38d.js" crossorigin="anonymous"></script>
+
     <!--FONTS-->
     <link rel="preconnect" href="https://fonts.googleapis.com"> 
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
     <link href="https://fonts.googleapis.com/css2?family=Handlee&family=Montserrat:wght@400;700&display=swap" rel="stylesheet"> 
 
     <!--STYLES-->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="Resources/CSS/style.css">
 </head>
-<body>
-    
-    <header>
+<body class="ballBody">
+
+<header>
+<nav class="navbar navbar-expand-md">
+        <div class="container-fluid">
+            <div class="menu-bloc-left">
+                <a class="navbar-brand" href="index.php" style="color: #f9e9fb; font-size: 30px; font-family: 'Handlee', cursive;">Jeux de hasard</a>
+            </div>
+            <div class="menu-bloc-right">
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#myMenu" aria-controls="myMenu" aria-expanded="false" aria-label="Menu for mobile">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="myMenu">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link" href="index.php" style="margin: 10px; color: #f9e9fb; font-size: 20px; font-family: 'Handlee', cursive;">Accueil</a>
+                        <a class="nav-item nav-link" href="lancer-de-des.php" style="margin: 10px; color: #f9e9fb; font-size: 20px; font-family: 'Handlee', cursive;">Lancer de dé</a>
+                        <a class="nav-item nav-link" href="boule-magique.php" style="margin: 10px; color: #f9e9fb; font-size: 20px; font-family: 'Handlee', cursive;">Boule magique</a>
+                        <a class="nav-item nav-link" href="#contact" style="margin: 10px; color: #f9e9fb; font-size: 20px; font-family: 'Handlee', cursive;">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<main class="ballMain">
+    <section class="home-title-ball" id="home-title-ball">
         <h1>Boule magique</h1>
         <h2>Posez votre question et la boule vous répondra...</h2>
-    </header>
+    </section>
 
-    <main>
+    <section class="form-ball" id="form-ball">
+
         <form action="index.php" method="POST">
-            <input type="text" name="question" placeholder="Votre question">
-            <input type="submit" name="responses" value="Réponse...">
+            <div class="question">
+                <input type="text" name="question" placeholder="Votre question">
+            </div>
+            <div class="submit">
+                <input type="submit" name="responses" value="Réponse...">
+            </div>
         </form>
         <?php
             if ($_POST) {
@@ -49,7 +83,31 @@
                 }
             }
         ?>
-    </main>
+    </section>
+</main>
+
+<footer id="contact" style="color: #f9e9fb;">
+    <div class="copyrights">
+        <p>Jeux de hasard - Copyright 2023.</p>
+    </div>
+    <div class="contact-details">
+        <div class="mail">
+            <i class="fa-solid fa-envelope"></i><p>contact@gmail.fr</p>
+        </div>
+        <div class="phoneNumber">
+            <i class="fa-solid fa-phone"></i><p>06 02 03 04 05</p>
+        </div>
+    </div>
+    <div class="socials">
+        <a href="https://github.com/Julia-Legrand/Jeux-de-hasard.git"><i class="fa-brands fa-github" style="color: #f9e9fb;"></i></a>
+        <a href="https://www.linkedin.com/"><i class="fa-brands fa-linkedin" style="color: #f9e9fb;"></i></a>
+        <a href="https://www.instagram.com"><i class="fa-brands fa-instagram" style="color: #f9e9fb;"></i></a>
+            
+    </div>
+</footer>
+
+<!--SCRIPT-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,28 +1,9 @@
-<?php
-    if ($_POST) {
-        $nbdes = $_POST['nbdes'];
-    }
-    else {
-        $nbdes = 0;
-    }
-
-    if ($_GET) {
-        $faces = $_GET['faces'];
-        for ($i=0 ; $i<$nbdes ; $i++) {
-            $results[] = rand (1,$faces);
-        }
-    }
-    else {
-        $faces = 6;
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lancer de dé</title>
+    <title>Application de jeux de hasard</title>
 
     <!--ICONS-->
     <script src="https://kit.fontawesome.com/d977b1f38d.js" crossorigin="anonymous"></script>
@@ -36,10 +17,10 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="Resources/CSS/style.css">
 </head>
-<body class="diceBody">
-    
+<body class="indexBody">
+
 <header>
-<nav class="navbar navbar-expand-md">
+    <nav class="navbar navbar-expand-md">
         <div class="container-fluid">
             <div class="menu-bloc-left">
                 <a class="navbar-brand" href="index.php" style="color: #f9e9fb; font-size: 30px; font-family: 'Handlee', cursive;">Jeux de hasard</a>
@@ -62,59 +43,23 @@
     </nav>
 </header>
 
-<main class="diceMain">
-    <section class="home-title-dice" id="home-title-dice">
-        <h1>Simulateur de lancer de dé</h1>
-        <h2>“Jouer, c'est expérimenter le hasard.”</h2>
+<main class="indexMain">
+    <section class="home-title-index">
+        <h1>Bienvenue sur mon application</h1>
+        <h2>Choisissez un jeu</h2>
     </section>
 
-    <nav class="navDice">
-        <ul>
-            <li><a href="lancer-de-des.php?faces=6">Dé à 6 faces</a></li>
-            <li><a href="lancer-de-des.php?faces=8">Dé à 8 faces</a></li>
-            <li><a href="lancer-de-des.php?faces=10">Dé à 10 faces</a></li>
-            <li><a href="lancer-de-des.php?faces=20">Dé à 20 faces</a></li>
-        </ul>
-    </nav>    
-    
-    <section class="tirage">
-        <h2>Tirage à <?= $faces ?> faces</h2>
-        
-        <div class="simulator">
-            <?php
-                for ($i=0 ; $i<$nbdes ; $i++) {
-                    if ($_POST) { ?>
-                    <div class='total'>
-                        <span><?php echo $results[$i] ?></span>
-                        </div>
-            <?php
-                    }
-                }
-                if (!$_POST) { ?>
-                    <div class='total'>
-                        <span></span>
-                    </div>
-            <?php
-                }
-            ?>
+    <section class="games">
+        <div class="diceGame">
+            <a href="lancer-de-des.php">Lancer de dé</a>
         </div>
-
-        <div class="submitForm">
-            <form action="lancer-de-des.php?faces=<?= $faces ?>" method="POST">
-                <label class="textForm" for="select">Choisissez le nombre de dés</label>
-                <select name="nbdes" class="nombreDes">
-                    <option value="1">1 dé</option>
-                    <option value="2">2 dés</option>
-                    <option value="3">3 dés</option>
-                </select>
-
-                <input type="submit" value="Lancez le dé">
-            </form>
+        <div class="ballGame">
+            <a href="boule-magique.php">Boule magique</a>
         </div>
     </section>
 </main>
 
-<footer id="contact" style="color: #f9e9fb;">
+<footer id="contact">
     <div class="copyrights">
         <p>Jeux de hasard - Copyright 2023.</p>
     </div>
@@ -127,9 +72,9 @@
         </div>
     </div>
     <div class="socials">
-        <a href="https://github.com/Julia-Legrand/Jeux-de-hasard.git"><i class="fa-brands fa-github" style="color: #f9e9fb;"></i></a>
-        <a href="https://www.linkedin.com/"><i class="fa-brands fa-linkedin" style="color: #f9e9fb;"></i></a>
-        <a href="https://www.instagram.com"><i class="fa-brands fa-instagram" style="color: #f9e9fb;"></i></a>
+        <a href="https://github.com/Julia-Legrand/Jeux-de-hasard.git"><i class="fa-brands fa-github"></i></a>
+        <a href="https://www.linkedin.com/"><i class="fa-brands fa-linkedin"></i></a>
+        <a href="https://www.instagram.com"><i class="fa-brands fa-instagram"></i></a>
             
     </div>
 </footer>
